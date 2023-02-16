dded6d34e24af86a61b18672ba070c4b8577aa1a
Romeo Valentin <romeov@stanford.edu>
2023-02-15 19:16:42 -0800

    Add VisualizationCallback

---
+++ src/callbacks.jl
@@ -0,0 +1,21 @@
+import FastAI, FastAI.FluxTraining
+import FastAI.FluxTraining: on
+using EllipsisNotation
+
+struct VisualizationCallback <: FluxTraining.Callback 
+  task
+  device
+end
+
+function FluxTraining.on(
+                ::FluxTraining.EpochEnd,
+                ::FluxTraining.Phases.AbstractValidationPhase,
+                cb::DisentanglingVAE.VisualizationCallback,
+                learner)
+  xs = first(learner.data[:validation]) .|> x->x[.., 1:1] |> cb.device
+  ys = learner.model(xs; apply_sigmoid=true);
+  FastAI.showoutputbatch(ShowText(), cb.task, cpu.(xs), cpu.(ys))
+end
+FluxTraining.stateaccess(::DisentanglingVAE.VisualizationCallback) = (data=FluxTraining.Read(), 
+                                                                      model=FluxTraining.Read(), )
+runafter(::VisualizationCallback) = (Metrics,)
