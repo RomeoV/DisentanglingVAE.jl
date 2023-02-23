b5ba7a125d5b8a2b049d4850badeedc518c1d120
Romeo Valentin <romeov@stanford.edu>
2023-02-22 19:18:39 -0800

    Move make_data_sample to tasks

--- src/train_vae.jl
+++ src/train_vae.jl
@@ -7,8 +7,6 @@ using FastAI.Flux: cpu, gpu
 import FastAI
 import FastAI.Flux
 import FastVision: ShowText, RGB
-import FixedPointNumbers: N0f8
-using Random: seed!
 using DisentanglingVAE: backbone, bridge, ResidualDecoder
 import ChainRulesCore: @ignore_derivatives
 import FastAI.Flux.MLUtils: _default_executor
@@ -17,27 +15,7 @@ import FastAI.MLUtils.Transducers: ThreadedEx
 _default_executor() = ThreadedEx()
 BE = ShowText();  # sometimes get segfault by default
 
-function make_data_sample(i)
-  seed!(i)
-  ks = rand(Bool, 6)
-  std = 1/sqrt(2)
-  v_lhs = randn(Float32, 6)*std
-  v_rhs = randn(Float32, 6)*std
-  v_rhs[ks] .= v_lhs[ks]
-
-  img_lhs = zeros(RGB{N0f8}, 64, 64)
-  DisentanglingVAE.draw!(img_lhs, v_lhs[1:2]..., RGB{N0f8}(1.,0,0))
-  DisentanglingVAE.draw!(img_lhs, v_lhs[3:4]..., RGB{N0f8}(0,1.,0))
-  DisentanglingVAE.draw!(img_lhs, v_lhs[5:6]..., RGB{N0f8}(0,0,1.))
-
-  img_rhs = zeros(RGB{N0f8}, 64, 64)
-  DisentanglingVAE.draw!(img_rhs, v_rhs[1:2]..., RGB{N0f8}(1.,0,0))
-  DisentanglingVAE.draw!(img_rhs, v_rhs[3:4]..., RGB{N0f8}(0,1.,0))
-  DisentanglingVAE.draw!(img_rhs, v_rhs[5:6]..., RGB{N0f8}(0,0,1.))
-
-  (img_lhs, v_lhs, img_rhs, v_rhs, ks)
-end
-data = mapobs(make_data_sample, 1:2^13)
+data = mapobs(DisentanglingVAE.make_data_sample, 1:2^13)
 
 task = DisentanglingVAETask()
 
