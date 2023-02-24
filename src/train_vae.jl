f2cc8167f7a6aec1d480955d587b69dfc5e5e4c4
Romeo Valentin <romeov@stanford.edu>
2023-02-24 15:26:45 -0800

    NIT: Don't load revise and train only 1 epoch

--- src/train_vae.jl
+++ src/train_vae.jl
@@ -1,4 +1,4 @@
-using Revise
+# using Revise
 using DisentanglingVAE
 
 using StatsBase: sample, mean
@@ -11,7 +11,7 @@ using DisentanglingVAE: backbone, bridge, ResidualDecoder
 import ChainRulesCore: @ignore_derivatives
 import FastAI.Flux.MLUtils: _default_executor
 import FastAI.MLUtils.Transducers: ThreadedEx
-import BSON
+import BSON: @save
 # ThreadPoolEx gave me problems, see https://github.com/JuliaML/MLUtils.jl/issues/142
 _default_executor() = ThreadedEx()
 BE = ShowText();  # sometimes get segfault by default
@@ -22,7 +22,7 @@ task = DisentanglingVAETask()
 
 BATCHSIZE=128
 dl, dl_val = taskdataloaders(data, task, BATCHSIZE, pctgval=0.1;
-                             buffer=false, partial=false,
+                             buffer=true, partial=false,
                             );
 
 DEVICE = gpu
@@ -43,10 +43,12 @@ learner = FastAI.Learner(model, ELBO;
 
 # test one input
 # @ignore_derivatives model(FastAI.getbatch(learner)[1] |> DEVICE)
-FastAI.fitonecycle!(learner, 500, 1e-4;
+FastAI.fitonecycle!(learner, 1, 1e-3;
+                    div=10,
+                    divfinal=1e2,
                     phases=(VAETrainingPhase() => dl,
                             VAEValidationPhase() => dl_val))
-model_cpu = cpu(model)
+model_cpu = cpu(model);
 @save "models/model2.bson" model_cpu
 #####################################################
 
