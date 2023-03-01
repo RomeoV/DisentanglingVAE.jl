8e9072a935cae328b47f2e0b3b69797f124fedc8
Romeo Valentin <romeov@astoria.stanford.edu>
2023-03-01 01:06:47 -0800

    Add fastai_patches file

---
+++ src/fastai_patches.jl
@@ -0,0 +1,19 @@
+import FastAI
+import FastAI: ShowBackend, AbstractBlockTask
+import FastAI: showblockinterpretable, getencodings, getblocks
+import FastAI: Continuous, ShowText
+import Printf: @sprintf
+
+# we patch to remove the dictionary structure
+# which causes a bug down the line I think
+function FastAI.showencodedsample(backend::ShowBackend, task::AbstractBlockTask, encsample::Tuple)
+    showblockinterpretable(backend,
+                           getencodings(task),
+                           getblocks(task).encodedsample,
+                           encsample)
+end
+
+# we patch to show vectors as columns
+function FastAI.showblock!(io, ::ShowText, block::Continuous, obs::AbstractArray)
+    print(io, join(round.(obs, sigdigits=3), '\n'))
+end
