7552f7c4275403412996c853ccc5a508c58a0b8e
Romeo Valentin <romeov@stanford.edu>
2023-05-24 16:42:55 -0700

    Train with manual iterations, and try to load/save task (unsuccesfully)

--- scripts/train_vae.jl
+++ scripts/train_vae.jl
@@ -13,13 +13,13 @@ import FastAI: TensorBoardBackend, LogMetrics, LogHyperParams
 import FluxTraining
 import FluxTraining: Checkpointer
 import Flux: cpu, gpu
-import FluxTraining: fit!
+import FluxTraining: fit!, epoch!
 import Wandb: WandbBackend
 import FastVision: ShowText, RGB
 import ChainRulesCore: @ignore_derivatives
 import MLUtils: _default_executor
 import MLUtils.Transducers: ThreadedEx
-import BSON: @save
+import BSON: @save, @load
 import Distributions: Normal
 @info "Starting train_vae.jl script"
 # ThreadPoolEx gave me problems, see https://github.com/JuliaML/MLUtils.jl/issues/142
@@ -34,7 +34,7 @@ function main(; model_path=nothing)
     task, model = if isnothing(model_path)
         task = DisentanglingVAETask()
 
-        dim_content, dim_style = 6, 2
+        dim_content, dim_style = 6, 0
 
         model = VAE(ResidualEncoder(128),  # <- output dim
                     bridge(128, dim_content+dim_style),
@@ -42,7 +42,9 @@ function main(; model_path=nothing)
                     DEVICE);
         (task, model)
     else
-        loadtaskmodel(model_path)
+        # loadtaskmodel(model_path)
+        @load model_path model;
+        DisentanglingVAETask(), model |> DEVICE
     end
 
 
@@ -50,7 +52,7 @@ function main(; model_path=nothing)
 
     ((cifar10_x, cifar10_y), blocks) = load(datarecipes()["cifar10"])
     airplane_img(i) = cifar10_x[(i % 15_000)+1]
-    make_data_sample_(i::Int) = make_data_sample(Normal, i;
+    make_data_sample_(i::Int) = make_data_sample(Normal(0, 0.5), i;
                                                  x0_fn = i->0.1*airplane_img(i))  # 15_000 airplanes
     data = mapobs(make_data_sample_, 1:n_datapoints)
 
@@ -70,17 +72,22 @@ function main(; model_path=nothing)
                     optimizer=opt,
                     data=(dl, dl_val),
                     callbacks=[FastAI.ToGPU(),
-                                FastAI.ProgressPrinter(),
-                                DisentanglingVAE.VisualizationCallback(task, gpu),
-                                DisentanglingVAE.LinearModelCallback(gpu, ),
-                                LogMetrics((tb_backend, csv_backend, wandb_backend)),
-                                ExpDirPrinterCallback(EXP_PATH),
-                                Checkpointer(EXP_PATH)])
+                               FastAI.ProgressPrinter(),
+                               DisentanglingVAE.VisualizationCallback(task, gpu),
+                               DisentanglingVAE.LinearModelCallback(gpu, ),
+                               LogMetrics((tb_backend, csv_backend, wandb_backend)),
+                               ExpDirPrinterCallback(EXP_PATH),
+                               Checkpointer(EXP_PATH)])
 
     # test one input
     # @ignore_derivatives model(FastAI.getbatch(learner)[1] |> DEVICE)
     n_epochs=(DRY ? 200 : 1000)
-    fit!(learner, n_epochs, (VAETrainingPhase()=>dl, VAEValidationPhase()=>dl_val))
+    # fit!(learner, n_epochs, (VAETrainingPhase()=>dl, VAEValidationPhase()=>dl_val))
+    for i in 1:n_epochs
+      epoch!(learner, VAETrainingPhase(), dl)
+      epoch!(learner, VAEValidationPhase(), dl_val)
+      GC.gc()
+    end
     # for _ in 1:4
     #     fitonecycle!(learner, 20, 3e-4;
     #                  phases=(VAETrainingPhase()   => dl,
